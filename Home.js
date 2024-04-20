585cdf0371538e3e323b8547f4638228a041dba4
Timothy <timkhumpan@gmail.com>
2024-04-20 11:17:55 -0400

    camera button

--- Home.js
+++ Home.js
@@ -1,8 +1,39 @@
 import React from "react";
-import { View, Text, Image, ScrollView, TextInput, Button } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { RNCamera } from "react-native-camera";
 
 const Home = () => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: "column",
+      backgroundColor: "black",
+    },
+    preview: {
+      flex: 1,
+      justifyContent: "flex-end",
+      alignItems: "center",
+    },
+    capture: {
+      flex: 0,
+      backgroundColor: "#fff",
+      borderRadius: 5,
+      padding: 15,
+      paddingHorizontal: 20,
+      alignSelf: "center",
+      margin: 20,
+    },
+  });
+
   takePicture = async function (camera) {
     const options = { quality: 0.5, base64: true };
     const data = await camera.takePictureAsync(options);
@@ -37,6 +68,7 @@ const Home = () => {
           ref={(ref) => {
             this.camera = ref;
           }}
+          style={styles.preview}
           type={RNCamera.Constants.Type.back}
           flashMode={RNCamera.Constants.FlashMode.on}
           androidCameraPermissionOptions={{
@@ -55,6 +87,16 @@ const Home = () => {
             console.log(barcodes);
           }}
         />
+        <View
+          style={{ flex: 0, flexDirection: "row", justifyContent: "center" }}
+        >
+          <TouchableOpacity
+            onPress={this.takePicture.bind(this)}
+            style={styles.capture}
+          >
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </ScrollView>
   );
