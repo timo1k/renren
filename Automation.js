2d0361c51fe9ea17c10292accd3df36226f6f8cd
Timothy <timkhumpan@gmail.com>
2024-04-20 19:39:47 -0400

    image

--- Automation.js
+++ Automation.js
@@ -1,24 +1,46 @@
 import React from "react";
 import {
   View,
-  Text,
   Image,
   ScrollView,
-  TextInput,
-  Button,
   StyleSheet,
-  TouchableOpacity,
+  Dimensions,
+  Text,
 } from "react-native";
+
+const screenWidth = Dimensions.get("window").width;
 
 const Automation = () => {
   return (
-    <Text>
-      {" "}
-      <Text>
-        {"\n\n\n\n"}AUTOMATION _ NOTHING{"\n"}
-      </Text>
-    </Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View>
+        <Text>{"\n\n\n\n"}</Text>
+        <Text>{"\n\n\n\n"}</Text>
+        <Text>{"\n\n\n\n"}</Text>
+        <Text>{"\n\n\n\n"}</Text>
+        <Text>{"\n\n\n\n"}</Text>
+      </View>
+
+      <View>
+        <Image
+          source={require("./assets/need2.png")}
+          style={{
+            width: screenWidth,
+            aspectRatio: 4 / 3, // Set the aspect ratio of your image
+            resizeMode: "contain", // Use 'contain' to fit the entire image within the width of the screen
+          }}
+        />
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Automation;
--- Profile.js
+++ Profile.js
@@ -1,24 +1,44 @@
 import React from "react";
 import {
   View,
-  Text,
   Image,
   ScrollView,
-  TextInput,
-  Button,
   StyleSheet,
-  TouchableOpacity,
+  Dimensions,
+  Text,
 } from "react-native";
+
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
 
 const Profile = () => {
   return (
-    <Text>
-      {" "}
-      <Text>
-        {"\n\n\n\n"}PROFILE _ NOTHING{"\n"}
-      </Text>
-    </Text>
+    <ScrollView contentContainerStyle={styles.container}>
+      {/* <Text>
+        {
+          "n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+        }
+      </Text> */}
+      <Text>{"\n\n\n\n"}</Text>
+
+      <Image
+        source={require("./assets/need.png")}
+        style={{
+          width: screenWidth,
+          height: screenHeight,
+          resizeMode: "contain",
+        }} // Adjust the size as needed
+      />
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Profile;
