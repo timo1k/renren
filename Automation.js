d6901043d46a7085d05d42d0c87e4f8a6018e5c5
tun08131 <tun08131@temple.edu>
2024-04-20 20:10:21 -0400

    page

--- Automation.js
+++ Automation.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Dimensions,
   Text,
+  TextInput
 } from "react-native";
 
 const screenWidth = Dimensions.get("window").width;
@@ -14,22 +15,17 @@ const Automation = () => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View>
-        <Text>{"\n\n\n\n"}</Text>
-        <Text>{"\n\n\n\n"}</Text>
-        <Text>{"\n\n\n\n"}</Text>
-        <Text>{"\n\n\n\n"}</Text>
-        <Text>{"\n\n\n\n"}</Text>
-      </View>
-
-      <View>
-        <Image
-          source={require("./assets/need2.png")}
-          style={{
-            width: screenWidth,
-            aspectRatio: 4 / 3, // Set the aspect ratio of your image
-            resizeMode: "contain", // Use 'contain' to fit the entire image within the width of the screen
-          }}
-        />
+        <View>
+          <Image
+            source={require("./assets/need2.png")}
+            style={{
+              width: screenWidth,
+              aspectRatio: 4 / 3,
+              resizeMode: "contain",
+            }}
+          />
+        </View>
+      
       </View>
     </ScrollView>
   );
@@ -37,10 +33,10 @@ const Automation = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
+  }
 });
 
 export default Automation;
